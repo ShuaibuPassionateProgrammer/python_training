46ac85cc55b9e3f7c926f12ce03a1a4f445ca0de
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-13 16:16:46 +0100

    First commit on exercise001.py

---
+++ exercise001.py
@@ -0,0 +1,7 @@
+def assignment(name, age):
+    print("Hey " + name + " you are " + str(age) + " years old")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+assignment(name, age)
