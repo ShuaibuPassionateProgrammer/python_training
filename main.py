53b5b6c4a49ce9f56ec7e8bff39aa447011e0052
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-13 16:15:48 +0100

    First commit on main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+firstname = ["Shuaibu", "Yusuf", "Usman", "Dauda"]
+lastname = ["Buhari", "Umar", "Musa", "Inusa"]
+
+for i, j in zip(firstname, lastname):
+    print(i, j)
+
+
+def sms():
+    print(142365)
+
+sms()
+
+def order():
+    print("I'm ordering something")
+
+order()
+
+from math import pow
+
+print(pow(2, 5))
+
+
+
+
+def power(a, b):
+    return a ** b
+
+print(power(2, 5))
+
+
+
+def greeting(name, age):
+    print("Hey " + name + " you are " + str(age) + " years old")
+
+
+#greeting("Abubakar")
+greeting("Imrana", 10)
+
+
+
+
+
+
+
+print("       /\\        ")
+print("      /  \\       ")
+print("     /    \\      ")
+print("    /      \\     ")
+print("   /        \\    ")
+print("  /          \\   ")
+print(" /            \\  ")
+print("/______________\\ ")
