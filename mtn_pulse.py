8e27c37657f07ccb28d98542299c7ae9d621c48f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-20 08:17:06 +0100

    Exit func is invoked

--- mtn_pulse.py
+++ mtn_pulse.py
@@ -1,14 +1,24 @@
 import os
-import main_menu
+import sys
 
 ussd = input("Please dial a USSD code: ")
 
 def clr():
     os.system("cls" if os.name == "nt" else "clear")
 
+def main_menu():
+    print("1. Join Pulse")
+    print("2. 1.2GB+1hr(YT/IG/TT) @N750")
+    print("3. Nightlife Bundles")
+    print("4. IG/Tiktok/Youtube Bundles")
+    print("5. Goodybag")
+    print("6. 750MB @N450")
+    print("7. Pulse Points")
+    print("8. Campus Zone")
+
 if ussd == "*406#":
     clr()
-    main_menu.main_menu()
+    main_menu()
     choice = input("")
 
     if choice == "1":
@@ -22,6 +32,12 @@ if ussd == "*406#":
         if choice1 == "1":
             clr()
             print("Dear Customer, you are welcome to mtn pulse. You can now enjoy calls @11KB per seconds, also you can use Night Bundle feature starting from (11:00PM - 06:00AM). Thank you")
+        elif choice1 == "2":
+            clr()
+            sys.exit(0)
+        elif choice1 == "0":
+            clr()
+            main_menu()
 
 else:
     clr()
