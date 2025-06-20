1ef6e3498a8ea656a23d04ecffd8bb7ef9c0e827
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-20 08:06:46 +0100

    main_menu.py file is created, exported and imported

---
+++ main_menu.py
@@ -0,0 +1,9 @@
+def main_menu():
+    print("1. Join Pulse")
+    print("2. 1.2GB+1hr(YT/IG/TT) @N750")
+    print("3. Nightlife Bundles")
+    print("4. IG/Tiktok/Youtube Bundles")
+    print("5. Goodybag")
+    print("6. 750MB @N450")
+    print("7. Pulse Points")
+    print("8. Campus Zone")
