a3bbd44e2e24dc389d899bc4ae7eed20905a54d2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-20 08:07:20 +0100

    main_menu.py is imported inside the mtn_pulse.py

--- mtn_pulse.py
+++ mtn_pulse.py
@@ -1,23 +1,14 @@
 import os
+import main_menu
 
 ussd = input("Please dial a USSD code: ")
 
 def clr():
     os.system("cls" if os.name == "nt" else "clear")
 
-def main_menu():
-    clr()
-    print("1. Join Pulse")
-    print("2. 1.2GB+1hr(YT/IG/TT) @N750")
-    print("3. Nightlife Bundles")
-    print("4. IG/Tiktok/Youtube Bundles")
-    print("5. Goodybag")
-    print("6. 750MB @N450")
-    print("7. Pulse Points")
-    print("8. Campus Zone")
-
 if ussd == "*406#":
-    main_menu()
+    clr()
+    main_menu.main_menu()
     choice = input("")
 
     if choice == "1":
